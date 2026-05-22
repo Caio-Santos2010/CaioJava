9c2d994a69f20f100aa4e217b70043502aabc680
Caio Santos <caio.santos.amaral@escola.pr.gov.br>
2026-05-22 11:17:44 -0300

    Add files via upload

---
+++ Programa.java
@@ -0,0 +1,10 @@
+public class Programa {
+    public static void main(String[] args) {
+        String nome = "Caio";
+        int idade = 15;
+        double altura = 1.83;
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+    }
+}
