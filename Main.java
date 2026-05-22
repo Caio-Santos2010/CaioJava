84ffe95011a3431b9437ce6b90fbae57bb6045df
Caio Santos <caio.santos.amaral@escola.pr.gov.br>
2026-05-22 10:49:08 -0300

    Caio Java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-public class Main {
+public class main {
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
+        String nome = "Caio";
+        int idade = 15;
+        double altura = 1,83;
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
     }
 }
